4c5dd4d048d87e2f0d49a1829719cab9346d7700
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2021-11-21 00:50:57 -0800

    updated trace of test2

--- test_program2.s
+++ test_program2.s
@@ -1,17 +1,17 @@
 # test2.asm
 #Assembly Code
 main:
-	ori	$t0, $0,  0x8000	# $t0 = 0x8000
-	addi	$t1, $0,  -32768	# $t1 = 0xfffff332
-	ori	$t2, $t0, 0x8001	# $t2 = 0xfffff332
+	ori	$t0, $0,  0x8000	# $t0 = 0x00008000
+	addi	$t1, $0,  -32768	# $t1 = 0xffff8000
+	ori	$t2, $t0, 0x8001	# $t2 = 0x00008001
 	beq	$t0, $t1, there		# not taken
 	slt	$t3, $t1, $t0		# $t3 = 1
 	bne	$t3, $0, here		# taken
 	j	there	    # doesn't execute
 here:
-	sub	$t2, $t2, $t0		# $t2 = 0xffff7332
+	sub	$t2, $t2, $t0		# $t2 = 0xffffffff
 	ori	$t0, $t0, 0xFF		# $t0 = 0x000080ff
 there:	
-	add	$t3, $t3, $t2		# $t3 = 0xffff7333
-	sub	$t0, $t2, $t0		# $t0 = 0xffff7233
-	sw	$t0, 82($t3)		# [0xffff7385] = 0xffff7233
+	add	$t3, $t3, $t2		# $t3 = 0x00008002
+	sub	$t0, $t2, $t0		# $t0 = 1
+	sw	$t0, 82($t3)		# [0x00008054] = 1
